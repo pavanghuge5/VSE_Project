413e8c8cb73bee592d10b075be2e539c260520fd
adamwarlock1248 <tejassamrut@gmail.com>
2024-08-05 23:14:24 +0530

    DotNET REST API Created for User Login & User Registration as Seller or Buyer and .gitignore file for .NET projects created.

---
+++ VSEscrowMgmtApp/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VSEscrowMgmtApp.Models;
+
+public partial class Category
+{
+    public int CatId { get; set; }
+
+    public string? CatName { get; set; }
+
+    public virtual ICollection<SubCategory> SubCategories { get; set; } = new List<SubCategory>();
+}
---
+++ VSEscrowMgmtApp/Models/Verification.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VSEscrowMgmtApp.Models;
+
+public partial class Verification
+{
+    public int? PaymentId { get; set; }
+
+    public int? SId { get; set; }
+
+    public int? BId { get; set; }
+
+    public string? BuyrConf { get; set; }
+
+    public string? SellrConf { get; set; }
+
+    public virtual Buyer? BIdNavigation { get; set; }
+
+    public virtual Payment? Payment { get; set; }
+
+    public virtual Seller? SIdNavigation { get; set; }
+}
